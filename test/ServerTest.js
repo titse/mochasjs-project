eaf18c3dbb7c58a54835fff0deff9a2cc0a4a774
Tiffany Tse <ttse93@gmail.com>
2017-06-08 15:17:56 -0700

    rename test/server.js to test/ServerTest.js

--- test/ServerTest.js
+++ test/ServerTest.js
@@ -47,20 +47,3 @@ describe("Color Code Converter API", function() {
   });
 
 });
-
-var http = require('http');
-
-this.server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world!\n');
-});
-
-exports.listen = function () {
-  this.server.listen.apply(this.server, arguments);
-};
-
-exports.close = function (callback) {
-  this.server.close(callback);
-};
-
-
